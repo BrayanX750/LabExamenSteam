9d30c551aa9255e46ffe69ba99224f8f33808f86
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-09-26 09:05:44 -0600

    inicio de archivos

--- src/steamjava/Steam.java
+++ src/steamjava/Steam.java
@@ -4,10 +4,52 @@
  */
 package steamjava;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 /**
  *
  * @author Usuario
  */
 public class Steam {
+
+    private RandomAccessFile codeFile;
+    private RandomAccessFile gamesFile;
+    private RandomAccessFile playerFile;
+
+    public static final int GAME_SIZE = 163;
+    public static final int PLAYER_SIZE = 249;
+
+    private void inicializar() {
+        try {
+            File steamCar = new File("steam");
+            if (!steamCar.exists()) {
+                steamCar.mkdir();
+            }
+            File downCar = new File("steam/downloads");
+            if (!downCar.exists()) {
+                downCar.mkdir();
+            }
+
+            codeFile = new RandomAccessFile("steam/codes.stm", "rw");
+            gamesFile = new RandomAccessFile("steam/games.stm", "rw");
+            playerFile = new RandomAccessFile("steam/player.stm", "rw");
+
+            if (codeFile.length() == 0) {
+                codeFile.writeInt(1);
+                codeFile.writeInt(1);
+                codeFile.writeInt(1);
+            }
+        } catch (IOException ex) {
+            System.out.println("ERROR en archivos base");
+        }
+
+    }
     
+    
+    public Steam(){
+        inicializar();
+    }
+
 }
